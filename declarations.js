ae16a32cd33f2d6d0a1d7e0d283bb637a77f2be5
Hanim <haniminah345@gmail.com>
2025-09-19 09:46:41 +0300

    Add files via upload

---
+++ declarations.js
@@ -0,0 +1,15 @@
+{
+	var name= 'hanim';
+var name= 'hanan';// re declaration + re assignment works with var.
+console.log(name)
+
+let age= 19;
+ age= 20;//when working with let, we can re assign but not re declare.
+ let age= 20;//this doesn't work because age is already declared.
+console.log(age)
+
+const uni= 'AASTU';
+uni= 'AAU';//but here, neither re assigning nor re declaring works.
+console.log(uni)
+
+ };
